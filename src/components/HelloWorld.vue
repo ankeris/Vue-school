e80470b1ec372527970201f964dd02dc28b718a9
Juozas Rastenis <juozas.rastenis@gmail.com>
2019-01-12 11:36:50 +0100

    components inherited and separated through route

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,23 @@
 <template>
-  <div>
-    <div v-for="thread in threads" class="col-large push-top">
-      <h2>{{thread.title}}</h2>
-      <div class="post-list">
-        <div class="post" v-for="postId in thread.posts">
-
-          <div class="user-info">
-            <a class="user-name" href="#">{{users[posts[postId].userId].name}}</a>
-            <a href="#">
-              <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt="">
-            </a>
-            <p class="desktop-only text-small">aaa</p>
-          </div>
-
-          <div class="post-content">
-            <div>
-              {{posts[postId].text}}
-            </div>
-          </div>
-
-          <div class="post-date text-faded">
-            {{posts[postId].publishedAt}}
-          </div>
-        </div>
-      </div>
-    </div>
+  <div class="col-full">
+      <ThreadList
+      :threads="threads"
+      ></ThreadList>
   </div>
 </template>
 
 <script>
   import sourceData from '../data.json';
+  import ThreadList from './ThreadList';
 
 export default {
       name: 'HelloWorld',
+      components: {
+          ThreadList,
+      },
       data() {
           return {
-              threads: sourceData.threads,
+              threads: Object.values(sourceData.threads),
               posts: sourceData.posts,
               users: sourceData.users,
           };
---
+++ src/components/ThreadListItem.vue
@@ -0,0 +1,45 @@
+<template>
+    <div v-if="thread" class="thread">
+        <div>
+            <p>
+            <router-link :to="{name: 'ThreadShow', params: {id: thread['.key']}}">
+                {{thread.title}}
+                </router-link>
+            </p>
+            <p class="text-faded text-xsmall">
+                By <a href="#">{{user.name}}, {{thread.publishedAt}}</a>.
+            </p>
+        </div>
+
+        <div class="activity">
+            <p class="replies-count">
+                {{countPosts}}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+    import sourceData from '../data.json';
+
+    export default {
+        props: {
+            thread: {
+                required: true,
+                type: Object,
+            },
+        },
+        computed: {
+            countPosts() {
+                return Object.keys(this.thread.posts).length - 1;
+            },
+            user() {
+                return sourceData.users[this.thread.userId];
+            },
+        },
+    };
+</script>
+
+<style>
+
+</style>
