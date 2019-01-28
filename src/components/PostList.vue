44f53b89d89156b45f581e9e0c7d958bb0f3ce8a
Juozas Rastenis <juozas.rastenis@gmail.com>
2019-01-28 22:04:47 +0100

    components split

---
+++ src/components/PostList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="post-list">
+        <PostListItem 
+            v-for="post in posts"
+            :key="post['.key']"
+            :post="post"
+        />
+    </div>
+</template>
+
+<script>
+import PostListItem from './PostListItem';
+
+export default {
+    components: {
+        PostListItem,
+    },
+    props: {
+        posts: {
+            type: Array,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style scoped>
+    
+</style>
--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -1,34 +1,20 @@
 <template>
     <div class="col-large push-top">
         <h2>{{thread.title}}</h2>
-        <div class="post-list">
-            <div class="post" v-for="postId in thread.posts" v-bind:key="postId">
-                <div class="user-info">
-                    <a class="user-name" href="#">{{users[posts[postId].userId].name}}</a>
-                    <a href="#">
-                        <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt="">
-                    </a>
-                    <p class="desktop-only text-small">aaa</p>
-                </div>
-
-                <div class="post-content">
-                    <div>
-                        {{posts[postId].text}}
-                    </div>
-                </div>
-
-                <div class="post-date text-faded">
-                    {{posts[postId].publishedAt}}
-                </div>
-            </div>
-        </div>
+        <PostList
+        :posts="posts"
+        />
     </div>
 </template>
 
 <script>
-    import sourceData from '../data.json';
+    import PostList from '@/components/PostList';
+    import sourceData from '@/data.json';
 
     export default {
+        components: {
+            PostList,
+        },
         props: {
             id: {
                 required: true,
@@ -38,9 +24,13 @@
         data() {
             return {
                 thread: sourceData.threads[this.id],
-                posts: sourceData.posts,
-                users: sourceData.users,
             };
+        },
+        computed: {
+            posts() {
+                const postIds = Object.values(this.thread.posts);
+                return Object.values(sourceData.posts).filter(post => postIds.includes(post['.key']));
+            },
         },
     };
 </script>
